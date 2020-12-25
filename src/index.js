485fcabecb5d4828b2ed3d56e080f241f55bac3c
joaoandrepestre <joaoandre.pestre@poli.ufrj.br>
2020-12-25 22:01:26 +0100

    Implements age restriction modal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom";
 import { WrappedIntlProvider } from "./react-components/wrapped-intl-provider";
 import registerTelemetry from "./telemetry";
@@ -7,6 +10,8 @@ import "./utils/theme";
 import { HomePage } from "./react-components/home/HomePage";
 import "./assets/stylesheets/globals.scss";
 import { AuthContextProvider } from "./react-components/auth/AuthContext";
+import { FormattedMessage } from "react-intl";
+import styles from "./assets/stylesheets/modal.scss";
 
 registerTelemetry("/home", "Hubs Home Page");
 
@@ -14,10 +19,35 @@ const store = new Store();
 window.APP = { store };
 
 function Root() {
+  const [showAgeRestriction, setShowAgeRestriction] = useState(true);
+  const handleCloseAgeRestriction = () => setShowAgeRestriction(false);
+
   return (
     <WrappedIntlProvider>
       <AuthContextProvider store={store}>
         <HomePage />
+
+        <Modal
+          className={styles.modal}
+          show={showAgeRestriction}
+          onHide={handleCloseAgeRestriction}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <FormattedMessage id="age-restriction.title" />
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FormattedMessage id="age-restriction.body" />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button className={styles.modal_button} variant="primary" onClick={handleCloseAgeRestriction}>
+              <FormattedMessage id="age-restriction.button" />
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </AuthContextProvider>
     </WrappedIntlProvider>
   );
